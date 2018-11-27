7ea7bfdf351db8de1ef785db750a1fd1fe0b62a2
Villars Gimm <villars.gimm@gmail.com>
2018-11-27 12:29:38 +0100

    Initial Commit

---
+++ Smash-up-macos/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Smash-up-macos
+//
+//  Created by Villars Gimm on 27/11/2018.
+//  Copyright © 2018 RibEye Studios. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
